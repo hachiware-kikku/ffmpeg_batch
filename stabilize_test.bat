df6f86cfa9e82392b7fd0f81d3931b57d1736fbe
hiro <kururi__@hotmail.com>
2021-09-05 15:26:06 +0900

    update

--- stabilize_test.bat
+++ stabilize_test.bat
@@ -1,117 +1,154 @@
 @echo off
 
 @rem ###########################################################
-@rem # 変数定義
+@rem # �ϐ���`
 @rem ###########################################################
 
-@rem ##### 1パス解析用のオプション #################################
+@rem ##### �f�B���N�g����` #######################################
+set PLAIN_000=G:\000_plain
+set SPEEDUP_003=G:\300_speedup
+set STABILIZE_006=G:\600_stabilize
+set COMPLETE_900=G:\900_complete
+
+@rem ##### 1�p�X��͗p�̃I�v�V���� #################################
 @REM # shakiness
-@REM # どれだけ映像が振れているかの設定：既定値は 5
-@REM # 値が大きいほど大きく振れている：1 から 10 まで
+@REM # �ǂꂾ���f�����U��Ă��邩�̐ݒ�F����l�� 5
+@REM # �l���傫���قǑ傫���U��Ă���F1 ���� 10 �܂�
 set SHAKINESS=10
 @REM # accuracy
-@REM # どれだけ振れを検出するかの精度：既定値は 15
-@REM # 値が大きいほど精度が高い：1 から 15 まで
+@REM # �ǂꂾ���U������o���邩�̐��x�F����l�� 15
+@REM # �l���傫���قǐ��x�������F1 ���� 15 �܂�
 set ACCURACY=15
 @REM # stepsize
-@REM # 何ピクセル単位で調べるか：既定値は 6
-@REM # 値が小さいほど精度が高い：1 から 32 まで
+@REM # ���s�N�Z���P�ʂŒ��ׂ邩�F����l�� 6
+@REM # �l���������قǐ��x�������F1 ���� 32 �܂�
 set STEPSIZE=1
 @REM # mincontrast
-@REM # 最小コントラスト比：既定値は 0.25
-@REM # 大きな値は振れを検出しなくなる：0 から 1 までの小数点を含む値
+@REM # �ŏ��R���g���X�g��F����l�� 0.25
+@REM # �傫�Ȓl�͐U������o���Ȃ��Ȃ�F0 ���� 1 �܂ł̏����_���܂ޒl
 set MICONTRAST=0
 @REM set MICONTRAST=1
 @REM # tripod
-@REM # tripod（三脚）モード：既定値は 0
-@REM # 持ち歩く時ではなく三脚を使った場合の振れを補正する：1 で有効化
+@REM # tripod�i�O�r�j���[�h�F����l�� 0
+@REM # �����������ł͂Ȃ��O�r���g�����ꍇ�̐U���␳����F1 �ŗL����
 set TRIPOD_1PASS=0
 
-@REM ##### 2パス補正用のオプション #################################
+@REM ##### 2�p�X�␳�p�̃I�v�V���� #################################
 @REM # smoothing
-@REM # 振れを調べる先読みフレーム数：既定値は 15
-@REM # 現在のフレームと、指定した値の前後フレームを参照する：0 から 1000 までの整数値
-@REM # 大きな値ほどなめらかになるが、制限がある（詳細はよくわからない）
+@REM # �U��𒲂ׂ��ǂ݃t���[�����F����l�� 15
+@REM # ���݂̃t���[���ƁA�w�肵���l�̑O��t���[�����Q�Ƃ���F0 ���� 1000 �܂ł̐����l
+@REM # �傫�Ȓl�قǂȂ߂炩�ɂȂ邪�A����������i�ڍׂ͂悭�킩��Ȃ��j
 set SMOOTHING=1000
 @REM # optalgo
-@REM # アルゴリズムの指定：既定値は opt
-@REM # 0, opt グローバル最適化
-@REM # 1, gauss カメラの動きをローパス（ハイカット）でフィルタしてガウス処理を行う
-@REM # 2, avg カメラの動きを平均化する
+@REM # �A���S���Y���̎w��F����l�� opt
+@REM # 0, opt �O���[�o���œK��
+@REM # 1, gauss �J�����̓��������[�p�X�i�n�C�J�b�g�j�Ńt�B���^���ăK�E�X�������s��
+@REM # 2, avg �J�����̓����𕽋ω�����
 set OPTALGO=0
 @REM set OPTALGO=1
 @REM set OPTALGO=2
 @REM # maxshift
-@REM # 最大何ピクセルのフレームを対象とするか：既定値は -1：-1 から 500 までの整数値
-@REM # 変更しなくても良い
+@REM # �ő剽�s�N�Z���̃t���[����ΏۂƂ��邩�F����l�� -1�F-1 ���� 500 �܂ł̐����l
+@REM # �ύX���Ȃ��Ă��ǂ�
 @REM set MAXSHIFT=-1
 @REM set MAXSHIFT=0
 set MAXSHIFT=500
 @REM # maxangle
-@REM # ラジアン単位の最大角度：既定値は -1（制限なし）：-1 から 3.14まで
-@REM # 変更しなくても良い：-1 から 500 までの整数値
+@REM # ���W�A���P�ʂ̍ő�p�x�F����l�� -1�i�����Ȃ��j�F-1 ���� 3.14�܂�
+@REM # �ύX���Ȃ��Ă��ǂ��F-1 ���� 500 �܂ł̐����l
 set MAXANGLE=-1
 @REM set MAXANGLE=0
 @REM set MAXANGLE=3.14
 @REM set MAXANGLE=500
 @REM # crop
-@REM # 補正により映像のない境界をどうするか
-@REM # 0, keep（既定値） 周りの情報で埋め合わせる
-@REM # 1, black 黒色で表示する
-@REM # invert 揺れを反転するかどうか：既定値は 0
-@REM # 変更しなくても良い
+@REM # �␳�ɂ��f���̂Ȃ����E���ǂ����邩
+@REM # 0, keep�i����l�j ����̏��Ŗ��ߍ��킹��
+@REM # 1, black ���F�ŕ\������
+@REM # invert �h��𔽓]���邩�ǂ����F����l�� 0
+@REM # �ύX���Ȃ��Ă��ǂ�
 set CROP=0
 @REM set CROP=1
 @REM # relative
-@REM # 揺れの参照フレームを相対参照するか、絶対参照するか：既定値は 1（相対参照）
-@REM # 0 は絶対参照
+@REM # �h��̎Q�ƃt���[���𑊑ΎQ�Ƃ��邩�A��ΎQ�Ƃ��邩�F����l�� 1�i���ΎQ�Ɓj
+@REM # 0 �͐�ΎQ��
 @REM set RELATIVE=0
 set RELATIVE=1
 @REM # zoom
-@REM # 何パーセント拡大するか：既定値は 0
-@REM # 揺れが大きい場合に補正により情報がない映像が増え、それを回避するために拡大することで補正映像を表示させないようにできる。
-@REM 0未満で縮小、0より大きいと拡大。0は拡大せずそのままのサイズ：-100 から 100 まで
+@REM # ���p�[�Z���g�g�傷�邩�F����l�� 0
+@REM # �h�ꂪ�傫���ꍇ�ɕ␳�ɂ���񂪂Ȃ��f���������A�����������邽�߂Ɋg�傷�邱�Ƃŕ␳�f����\�������Ȃ��悤�ɂł���B
+@REM 0�����ŏk���A0���傫���Ɗg��B0�͊g�傹�����̂܂܂̃T�C�Y�F-100 ���� 100 �܂�
 set ZOOM=-100
 @REM set ZOOM=-50
 @REM set ZOOM=-75
 @REM set ZOOM=0
 @REM set ZOOM=100
 @REM # optzoom
-@REM # 上の zoom を使うと最適な拡大範囲に調整する：既定値は 1
-@REM # 0 使わない
-@REM # 1 固定ズーム。激しく揺れていると枠が見えることもある
-@REM # 2 可変ズーム。枠が見えなくなる。この下の zoomspeed を参照
+@REM # ��� zoom ���g���ƍœK�Ȋg��͈͂ɒ�������F����l�� 1
+@REM # 0 �g��Ȃ�
+@REM # 1 �Œ�Y�[���B�������h��Ă���Ƙg�������邱�Ƃ�����
+@REM # 2 �σY�[���B�g�������Ȃ��Ȃ�B���̉��� zoomspeed ���Q��
 @REM set OPTZOOM=0
 set OPTZOOM=0
 @REM set OPTZOOM=2
 @REM # zoomspeed
-@REM # 最大何パーセント、ズーム（拡大）するか：既定値は 0.25
-@REM # 大きい値ほどより拡大される：0 から 5 まで
+@REM # �ő剽�p�[�Z���g�A�Y�[���i�g��j���邩�F����l�� 0.25
+@REM # �傫���l�قǂ��g�傳���F0 ���� 5 �܂�
 set ZOOMSPEED=0
 @REM # interpol
-@REM # 補完処理のアルゴリズム
-@REM # 0, no 処理を行わない
-@REM # 1, linear 横方向の linear 処理を行う
-@REM # 2, bilinear（既定値） 縦と横の bilinear 処理を行う
-@REM # 3, bicubic 縦と横の cubic 処理（bicubic）を行う（低速度）
+@REM # �⊮�����̃A���S���Y��
+@REM # 0, no �������s��Ȃ�
+@REM # 1, linear �������� linear �������s��
+@REM # 2, bilinear�i����l�j �c�Ɖ��� bilinear �������s��
+@REM # 3, bicubic �c�Ɖ��� cubic �����ibicubic�j���s���i�ᑬ�x�j
 @REM set INTERPOL=0
 @REM set INTERPOL=1
 @REM set INTERPOL=2
 set INTERPOL=3
 @REM # tripod
-@REM # tripod（三脚）モード：既定値は 0
-@REM # vidstabdetect で tripod を使ったときに併用する
-@REM # 1 にすると relative=0:smoothing=0 になる
+@REM # tripod�i�O�r�j���[�h�F����l�� 0
+@REM # vidstabdetect �� tripod ���g�����Ƃ��ɕ��p����
+@REM # 1 �ɂ���� relative=0:smoothing=0 �ɂȂ�
 set TRIPOD_2PASS=0
 
-ffmpeg.exe -i testdata.mp4 -vf vidstabdetect=shakiness=%SHAKINESS%:accuracy=%ACCURACY%:stepsize=%STEPSIZE%:mincontrast=%MICONTRAST%:tripod=%TRIPOD_1PASS% -an -f null -
+@rem ###########################################################
+@rem # �����̊J�n
+@rem ###########################################################
+:MAIN
 
-ffmpeg.exe -i testdata.mp4 -vf ^
-vidstabtransform=smoothing=%SMOOTHING%:optalgo=%OPTALGO%:maxshift=%MAXSHIFT%:maxangle=%MAXANGLE%:crop=%CROP%:relative=%RELATIVE%:zoom=%ZOOM%:optzoom=%OPTZOOM%:^
-zoomspeed=%ZOOMSPEED%:interpol=%INTERPOL%:tripod=%TRIPOD_2PASS% ^
--y 1pass_%SHAKINESS%_%ACCURACY%_%STEPSIZE%_%MICONTRAST%_%TRIPOD_1PASS%_2pass_%SMOOTHING%_%OPTALGO%_%MAXSHIFT%_%MAXANGLE%_%CROP%_%RELATIVE%_%ZOOM%_%OPTZOOM%_%INTERPOL%_%TRIPOD_2PASS%.mp4
+@REM ���xUP�̑Ώۃt�@�C��
+FOR /R %PLAIN_000% %%i IN (*.mp4) do call :SPEEDUP "%%i"
+@REM �X�^�r���C�Y
 
-ffmpeg.exe -i testdata.mp4 -vf ^
-vidstabtransform=smoothing=%SMOOTHING%:optalgo=%OPTALGO%:maxshift=%MAXSHIFT%:maxangle=%MAXANGLE%:crop=%CROP%:relative=%RELATIVE%:zoom=%ZOOM%:optzoom=%OPTZOOM%:^
-zoomspeed=%ZOOMSPEED%:interpol=%INTERPOL%:tripod=%TRIPOD_2PASS%,unsharp ^
--y 1pass_%SHAKINESS%_%ACCURACY%_%STEPSIZE%_%MICONTRAST%_%TRIPOD_1PASS%_2pass_%SMOOTHING%_%OPTALGO%_%MAXSHIFT%_%MAXANGLE%_%CROP%_%RELATIVE%_%ZOOM%_%OPTZOOM%_%INTERPOL%_%TRIPOD_2PASS%_unsharp.mp4
+
+echo WAIT 500 sec
+timeout /t 500
+
+GOTO :MAIN
+
+@rem ###########################################################
+@rem # ���x��3�{�ɂ���
+@rem ###########################################################
+:SPEEDUP
+set INPUT_FILE=%~1
+set FILE_NAME=%~nx1
+echo INPUT_FILE: %INPUT_FILE%
+echo FILE_NAME: %FILE_NAME%
+ffmpeg.exe -i "%INPUT_FILE%" -vf setpts=PTS/3.0 -af atempo=3.0 -y "%FILE_NAME%"
+IF      ERRORLEVEL 1 MOVE /Y "%INPUT_FILE%" "%SPEEDUP_003%"
+IF NOT  ERRORLEVEL 1 MOVE /Y "%FILE_NAME%" "%SPEEDUP_003%"
+IF EXIST    "%INPUT_FILE%" DEL /F "%INPUT_FILE%"
+GOTO :EOF
+
+
+
+@REM ffmpeg.exe -i testdata.mp4 -vf vidstabdetect=shakiness=%SHAKINESS%:accuracy=%ACCURACY%:stepsize=%STEPSIZE%:mincontrast=%MICONTRAST%:tripod=%TRIPOD_1PASS% -an -f null -
+
+@REM ffmpeg.exe -i testdata.mp4 -vf ^
+@REM vidstabtransform=smoothing=%SMOOTHING%:optalgo=%OPTALGO%:maxshift=%MAXSHIFT%:maxangle=%MAXANGLE%:crop=%CROP%:relative=%RELATIVE%:zoom=%ZOOM%:optzoom=%OPTZOOM%:^
+@REM zoomspeed=%ZOOMSPEED%:interpol=%INTERPOL%:tripod=%TRIPOD_2PASS% ^
+@REM -y 1pass_%SHAKINESS%_%ACCURACY%_%STEPSIZE%_%MICONTRAST%_%TRIPOD_1PASS%_2pass_%SMOOTHING%_%OPTALGO%_%MAXSHIFT%_%MAXANGLE%_%CROP%_%RELATIVE%_%ZOOM%_%OPTZOOM%_%INTERPOL%_%TRIPOD_2PASS%.mp4
+
+@REM ffmpeg.exe -i testdata.mp4 -vf ^
+@REM vidstabtransform=smoothing=%SMOOTHING%:optalgo=%OPTALGO%:maxshift=%MAXSHIFT%:maxangle=%MAXANGLE%:crop=%CROP%:relative=%RELATIVE%:zoom=%ZOOM%:optzoom=%OPTZOOM%:^
+@REM zoomspeed=%ZOOMSPEED%:interpol=%INTERPOL%:tripod=%TRIPOD_2PASS%,unsharp ^
+@REM -y 1pass_%SHAKINESS%_%ACCURACY%_%STEPSIZE%_%MICONTRAST%_%TRIPOD_1PASS%_2pass_%SMOOTHING%_%OPTALGO%_%MAXSHIFT%_%MAXANGLE%_%CROP%_%RELATIVE%_%ZOOM%_%OPTZOOM%_%INTERPOL%_%TRIPOD_2PASS%_unsharp.mp4
